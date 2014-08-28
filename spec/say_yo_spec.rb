1e52ad7bb5c25f5fc4b1b86dfcc2f31dd110e686
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-28 17:59:53 +0100

    added random voices

--- spec/say_yo_spec.rb
+++ spec/say_yo_spec.rb
@@ -2,7 +2,8 @@ require 'say_yo'
 
 describe SayYo do 
 	it 'says "yo" (obviously)' do 
-		expect(SayYo).to receive(:system).with("say yo") 	
+    allow(SayYo).to receive(:random_voice).and_return('Vicki')
+		expect(SayYo).to receive(:system).with("say -v Vicki yo") 	
 		SayYo.yo
 	end
 end
